bc197b619e0d35bd46837f4e7023646734d5a16b
Gabe Stocco <gfs@pm.me>
2019-03-27 10:28:02 -0700

    Adding build scripts for Linux and Mac

--- Cli/build.ps1
+++ Cli/build.ps1
@@ -1,14 +1,13 @@
- param (
+param (
     [string]$release = "Debug"
- )
+)
 
 if ($release -eq "Debug"){
     dotnet build
 }
 if ($release -eq "Release"){
-    dotnet publish -r win10-x64 --self-contained true
+    dotnet publish -c Release -r win10-x64 --self-contained true
     if ($?) {
-        ..\Tools\warp-packer.exe --arch windows-x64 --input_dir bin\Debug\netcoreapp2.1\win10-x64\publish\ --exec AttackSurfaceAnalyzerCli.exe --output AttackSurfaceAnalyzerCli.exe
+        ..\Tools\warp-packer.exe --arch windows-x64 --input_dir bin\Release\netcoreapp2.1\win10-x64\publish\ --exec AttackSurfaceAnalyzerCli.exe --output AttackSurfaceAnalyzerCli.exe
     }
 }
-
