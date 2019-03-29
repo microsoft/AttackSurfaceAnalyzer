b9c86f0958b2d8efad712b5d8a6235ca7e2e5a80
Gabe Stocco <gfs@pm.me>
2019-03-29 13:34:20 -0700

    Update build instructions. Improve build scripts for CLI.

--- Cli/build_mac.sh
+++ Cli/build_mac.sh
@@ -12,6 +12,7 @@ if ["$release" = "Debug"); then
     dotnet build
 fi
 if ["$release" = "Release"]; then
-    dotnet publish -c Release -r osx-x64 --self-contained true && ../Tools/macos-x64.warp-packer --arch macos-x64 --input_dir bin/Release/netcoreapp2.1/osx-x64/publish/ --exec AttackSurfaceAnalyzerCli --output AttackSurfaceAnalyzerCli
+	version=`nbgv get-version -v AssemblyInformationalVersion`
+    dotnet publish -c Release -r osx-x64 --self-contained true && ../Tools/macos-x64.warp-packer --arch macos-x64 --input_dir bin/Release/netcoreapp2.1/osx-x64/publish/ --exec AttackSurfaceAnalyzerCli --output bin/AttackSurfaceAnalyzerCli-macos-$version.bin
     chmod +x AttackSurfaceAnalyzerCli
 fi
