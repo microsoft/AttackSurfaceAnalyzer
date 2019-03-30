15592b56f3c4cc30a339c8b8d0c319480d181e76
Gabe Stocco <gstocco@microsoft.com>
2019-03-30 11:57:57 -0700

    Add JSON Export to CLI
    Also fix Mac build scripts.
    Update all CLI build scripts to 2.2.

--- Cli/build_linux.sh
+++ Cli/build_linux.sh
@@ -14,7 +14,7 @@ if [ "$RELEASE" == "Debug" ]; then
     dotnet build
 elif [ "$RELEASE" == "Release" ]; then
     VERSION=$(nbgv get-version -v AssemblyInformationalVersion)
-    dotnet publish -c "$RELEASE" -r linux-x64 --self-contained true && ../Tools/linux-x64.warp-packer --arch linux-x64 --input_dir bin/Release/netcoreapp2.1/linux-x64/publish/ --exec AttackSurfaceAnalyzerCli --output bin/AttackSurfaceAnalyzerCli-linux-$VERSION.bin
-    chmod +x AttackSurfaceAnalyzerCli
+    dotnet publish -c "$RELEASE" -r linux-x64 --self-contained true && ../Tools/linux-x64.warp-packer --arch linux-x64 --input_dir bin/Release/netcoreapp2.2/linux-x64/publish/ --exec AttackSurfaceAnalyzerCli --output bin/AttackSurfaceAnalyzerCli-linux-$VERSION.bin
+    chmod +x bin/AttackSurfaceAnalyzerCli-linux-$VERSION.bin
     echo "Build completed, result is located at bin/AttackSurfaceAnalyzerCli-linux-$VERSION.bin"
 fi
