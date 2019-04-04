81bde911095c74e642982635362f80040ce7b274
Gabe Stocco <gstocco@microsoft.com>
2019-04-04 13:48:07 -0700

    Create TelemetryConfig.cs

---
+++ Lib/Utils/TelemetryConfig.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace AttackSurfaceAnalyzer.Utils
+{
+    public class TelemetryConfig
+    { 
+        public static readonly string InstrumentationKeyCli = "e44cb140-4cde-4973-a219-2106a560093d";
+        public static readonly string IntrumentationKeyGui = "79fc14e7-936c-4dcf-ba66-9a4da6e341ef";
+    }
+}
