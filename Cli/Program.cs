63739b6d9c1b71a2f0af1f111e73b0dc14a7bd42
Gabe Stocco <gfs@pm.me>
2019-04-03 17:11:40 -0700

    Fixes GUI collection.

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -1034,19 +1034,19 @@ namespace AttackSurfaceAnalyzer.Cli
 
             Filter.LoadFilters(opts.FilterLocation);
             Console.WriteLine("Before telemetry");
-            //Telemetry.Setup();
-            //Dictionary<string, string> StartEvent = new Dictionary<string, string>();
-            //StartEvent.Add("Version", Helpers.GetVersionString());
-            //StartEvent.Add("Files", opts.EnableFileSystemCollector.ToString());
-            //StartEvent.Add("Ports", opts.EnableNetworkPortCollector.ToString());
-            //StartEvent.Add("Users", opts.EnableUserCollector.ToString());
-            //StartEvent.Add("Certificates", opts.EnableCertificateCollector.ToString());
-            //StartEvent.Add("Registry", opts.EnableRegistryCollector.ToString());
-            //StartEvent.Add("Service", opts.EnableServiceCollector.ToString());
-            //StartEvent.Add("Files", opts.EnableFileSystemCollector.ToString());
-            //StartEvent.Add("Files", opts.EnableFileSystemCollector.ToString());
+            Telemetry.Setup();
+            Dictionary<string, string> StartEvent = new Dictionary<string, string>();
+            StartEvent.Add("Version", Helpers.GetVersionString());
+            StartEvent.Add("Files", opts.EnableFileSystemCollector.ToString());
+            StartEvent.Add("Ports", opts.EnableNetworkPortCollector.ToString());
+            StartEvent.Add("Users", opts.EnableUserCollector.ToString());
+            StartEvent.Add("Certificates", opts.EnableCertificateCollector.ToString());
+            StartEvent.Add("Registry", opts.EnableRegistryCollector.ToString());
+            StartEvent.Add("Service", opts.EnableServiceCollector.ToString());
+            StartEvent.Add("Files", opts.EnableFileSystemCollector.ToString());
+            StartEvent.Add("Files", opts.EnableFileSystemCollector.ToString());
 
-            //Telemetry.Client.TrackEvent("Begin collecting", StartEvent);
+            Telemetry.Client.TrackEvent("Begin collecting", StartEvent);
             Console.WriteLine("After telemetry");
 
             DatabaseManager.SqliteFilename = opts.DatabaseFilename;
