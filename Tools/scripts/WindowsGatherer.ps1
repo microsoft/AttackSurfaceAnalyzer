4935a7415057cc535d858bc80427cd15e0c84ad4
Gabe Stocco <gfs@pm.me>
2019-05-18 06:02:52 -0700

    Update to windows script.

--- Tools/scripts/WindowsGatherer.ps1
+++ Tools/scripts/WindowsGatherer.ps1
@@ -7,13 +7,15 @@ cd output
 Get-ChildItem -Recurse -Directory | ForEach-Object {
     $dir = $_
     Get-ChildItem -File -Filter *_* $_ | ForEach-Object{
-        Write-Host $(Get-content $_ | Measure-Object –Line)
         if ( $(Get-content $_ | Measure-Object –Line).Lines -gt 1){
             Write-Host "Parsing non-empty file $_"
-            $c = $_.split("-")[0]
-            $c = "windows_$c"
+            $c = "windows"
             $p = Get-Content ~/CREDENTIAL
-            $u = GET_CONTENT ~/USER
+            $u = Get-Content ~/USER
+            Get-Content .\TestObj.txt | Select-String -SimpleMatch -Pattern "path" | ForEach-Object{
+                    $a = $_.Line.Split('"')[1]
+                    Write-Host $a
+            }
             "C:\Program Files\MongoDb\4.0\bin\mongoimport.exe" -h noise-mongodb.documents.azure.com:10255 --ssl -c $c -u $u -p $p --jsonArray $f
         } else {
             Write-Host "Skipping empty file $_"
