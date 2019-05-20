cff5abb639f6081407e182301afb2c8acdbaf45e
gfs <gfs@pm.me>
2019-05-20 15:03:09 -0700

    Update WindowsGatherer.ps1

--- Tools/scripts/WindowsGatherer.ps1
+++ Tools/scripts/WindowsGatherer.ps1
@@ -4,27 +4,43 @@ AttackSurfaceAnalyzerCli.exe config --trim-to-latest
 
 cd output
 
+$p = Get-Content ~/CREDENTIAL
+$u = Get-Content ~/USER
+$c = "windows"
+
+
+$files = @()
+$registry = @()
+
+
 Get-ChildItem -Recurse -Directory | ForEach-Object {
     $dir = $_
-    Get-ChildItem -File -Filter *_* $_ | ForEach-Object{
-        if ( $(Get-content $_ | Measure-Object –Line).Lines -gt 1){
-            Write-Host "Parsing non-empty file $_"
-            $c = "windows"
-            $p = Get-Content ~/CREDENTIAL
-            $u = Get-Content ~/USER
-            Get-Content .\TestObj.txt | Select-String -SimpleMatch -Pattern "path" | ForEach-Object{
-                    $a = $_.Line.Split('"')[1]
-                    Write-Host $a
+    Get-ChildItem -File -Filter files_* $dir | ForEach-Object{
+        if ( $(Get-content $_.FullName | Measure-Object -Line).Lines -gt 1){
+            Get-Content $_.FullName | Select-String -SimpleMatch -Pattern "`"Path`"" | ForEach-Object{
+                $a = $_.Line.Split('`"')[3]
+                $files += "INSERT INTO FILES (Path) VALUES (`"$a`");"
             }
-            "C:\Program Files\MongoDb\4.0\bin\mongoimport.exe" -h noise-mongodb.documents.azure.com:10255 --ssl -c $c -u $u -p $p --jsonArray $f
-        } else {
-            Write-Host "Skipping empty file $_"
         }
     }
-    Delete-Item -Path $dir.FullName
-#    Write-Host "Deleting $dir"
+    
+    Get-ChildItem -File -Filter registry_* $_ | ForEach-Object{
+        if ( $(Get-content $_.FullName | Measure-Object -Line).Lines -gt 1){
+            Get-Content $_.FullName | Select-String -SimpleMatch -Pattern "`"Key`"" | ForEach-Object{
+                $a = $_.Line.Split('`"')[3]
+                $registry += "INSERT INTO REGISTRY (Key) VALUES (`"$a`");"
+            }
+        }
+    }
 }
+# Dedupe modified results which report the path twice
+$files = $files | sort -unique
+$registry = $registry | sort -unique
 
+Add-Content data.sql $files
+Add-Content data.sql $registry
 
+Get-Content data.sql | "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -h asa-noise-2.database.windows.net -u $u -p $p asa-noise 
+del data.sql
 
 cd ..
