9ce11e9d70426327c33aa00b0fe198ea3f5e132f
gfs <gfs@pm.me>
2019-05-20 18:29:24 -0700

    Improve performance
    Also now will skip "full" dumps, by estimating that collections greater than 50MB are erroneously a dump of all data. Appropriate for this data gathering exercise, but may not be appropriate in all cases.

--- Tools/scripts/WindowsGatherer.ps1
+++ Tools/scripts/WindowsGatherer.ps1
@@ -1,3 +1,5 @@
+﻿exit
+
 $wd = pwd
 
 if (-not (Test-Path ~\stage) ){
@@ -15,49 +17,72 @@ cd ~\stage
 ~\AsaCli\res\AttackSurfaceAnalyzerCli.exe export-collect --outputpath output --explodedoutput
 ~\AsaCli\res\AttackSurfaceAnalyzerCli.exe config --trim-to-latest
 
-
-
 $p = Get-Content ~/CREDENTIAL.txt
 $u = Get-Content ~/USER.txt
 $c = "windows"
 
-$files = @()
-$registry = @()
+
+$files = New-Object Collections.Generic.List[string]
+$registry = New-Object Collections.Generic.List[string]
+
 
 Get-ChildItem -Recurse -Directory output | ForEach-Object {
     $dir = $_.FullName
     Get-ChildItem -File -Filter files_* $dir | ForEach-Object{
-        if ( $(Get-content $_.FullName | Measure-Object -Line).Lines -gt 1){
-            Get-Content $_.FullName | Select-String -SimpleMatch -Pattern "`"Path`"" | ForEach-Object{
-                $a = $_.Line.Split('`"')[3]
-                $files += "$a"
+        if ( $(Get-Item $_.FullName).Length -lt 50000000 ){
+            if ( $(Get-Item $_.FullName).Length -gt 2){
+                Write-Host "Parsing $_"
+                Get-Content $_.FullName | Select-String -SimpleMatch -Pattern "`"Path`"" | ForEach-Object{
+                    $a = $_.Line.Split('`"')[3]
+                    $files.add($a)
+                }
+            }
+            else{
+                Write-Host "Skipping $_. Empty file."
             }
         }
+        else{
+            Write-Host "Skipping $_. Too large."
+        }
     }
-    $files = $files | sort -unique
-
-    Add-Content ~\files.txt $files
 
     Get-ChildItem -File -Filter registry_* $dir | ForEach-Object{
-        if ( $(Get-content $_.FullName | Measure-Object -Line).Lines -gt 1){
-            Get-Content $_.FullName | Select-String -SimpleMatch -Pattern "`"Key`"" | ForEach-Object{
-                $a = $_.Line.Split('`"')[3]
-                $registry += "$a"
+        if ( $(Get-Item $_.FullName).Length -lt 50000000 ){
+            if ( $(Get-Item $_.FullName).Length -gt 2){
+                Write-Host "Parsing $_"
+                Get-Content $_.FullName | Select-String -SimpleMatch -Pattern "`"Key`"" | ForEach-Object{
+                    $a = $_.Line.Split('`"')[3]
+                    $registry.add($a)
+                }
+            }
+            else{
+                Write-Host "Skipping $_. Empty file."
             }
         }
+        else{
+            Write-Host "Skipping $_. Too large."
+        }
     }
+
+    $files = $files | sort -unique
+    Add-Content ~\files.txt $files
 
     $registry = $registry | sort -unique
     Add-Content ~\registry.txt $registry
 
-    $registry=@()
-    $files=@()
+    if ($files.Count -gt 0){
+        C:\Program` Files\Microsoft` SQL` Server\Client` SDK\ODBC\110\Tools\Binn\bcp.exe windows_files in C:\users\noise\files.txt -S asa-noise-2.database.windows.net -d asa-noise -U $u -P $p -q -c -t
+    }
 
-    Move-Item -Path $dir -Destination ~\stage\ingested\
+    if ($registry.Count -gt 0){
+        C:\Program` Files\Microsoft` SQL` Server\Client` SDK\ODBC\110\Tools\Binn\bcp.exe registry in C:\users\noise\registry.txt -S asa-noise-2.database.windows.net -d asa-noise -U $u -P $p -q -c -t
+    }
+    
+    $files=New-Object Collections.Generic.List[string]
+    del C:\users\noise\files.txt
+     
+    $registry=New-Object Collections.Generic.List[string]
+    del C:\users\noise\registry.txt
+
+    Move-Item -Path $dir -Destination ~\stage\ingested\    
 }
-
-C:\Program` Files\Microsoft` SQL` Server\Client` SDK\ODBC\110\Tools\Binn\bcp.exe windows_files in ~\files.txt -S asa-noise-2.database.windows.net -d asa-noise -U $u -P $p -q -c -t
-C:\Program` Files\Microsoft` SQL` Server\Client` SDK\ODBC\110\Tools\Binn\bcp.exe registry in ~\registry.txt -S asa-noise-2.database.windows.net -d asa-noise -U $u -P $p -q -c -t
-
-del C:\users\noise\files.txt
-del C:\users\noise\registry.txt
