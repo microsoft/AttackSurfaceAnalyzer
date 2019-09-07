66261967e7820b6d0e67376213bccd0f8d6c5b46
Gabe Stocco <gstocco@microsoft.com>
2019-09-07 12:01:19 -0700

    Add firewall tests

--- AsaLibTests/AsaLibTests.cs
+++ AsaLibTests/AsaLibTests.cs
@@ -39,5 +39,96 @@ namespace AsaTests
             Dictionary<string, List<CompareResult>> results = bc.Results;
             Assert.IsTrue(results["FILES_CREATED"].Where(x => x.Identity.Contains(testFile)).Count() > 0);
         }
+
+        [TestMethod]
+        public void TestFirewallCollectorOSX()
+        {
+            if (RuntimeInformation.IsOsPlatform(OSPlatform.OSX))
+            {
+                Strings.Setup();
+
+                DatabaseManager.SqliteFilename = Path.GetTempFileName();
+                DatabaseManager.Setup();
+                var FirstRunId = "TestFirewallCollector-1";
+                var SecondRunId = "TestFirewallCollector-2";
+                var fwc = new FirewallCollector(FirstRunId);
+                fwc.Execute();
+
+                var result = ExternalCommandRunner.RunExternalCommand("/usr/libexec/ApplicationFirewall/socketfilterfw", "--add", "/bin/bash);
+
+                fwc = new FirewallCollector(SecondRunId);
+                fwc.Execute();
+
+                result = ExternalCommandRunner.RunExternalCommand("/usr/libexec/ApplicationFirewall/socketfilterfw", "--remove", "/bin/bash);
+
+                BaseCompare bc = new BaseCompare();
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                if (!bc.TryCompare(FirstRunId, SecondRunId))
+                {
+                    Assert.Fail();
+                }
+
+                Dictionary<string, List<CompareResult>> results = bc.Results;
+                Assert.IsTrue(results["FIREWALL_CREATED"].Where(x => x.Identity.Contains("/bin/bash")).Count() > 0);
+            }
+        }
+
+        [TestMethod]
+        public void TestFirewallCollectorWindows()
+        {
+            if (RuntimeInformation.IsOsPlatform(OSPlatform.Windows))
+            {
+                Strings.Setup();
+
+                DatabaseManager.SqliteFilename = Path.GetTempFileName();
+                DatabaseManager.Setup();
+                var FirstRunId = "TestFirewallCollector-1";
+                var SecondRunId = "TestFirewallCollector-2";
+                var fwc = new FirewallCollector(FirstRunId);
+                fwc.Execute();
+
+                var rule = FirewallManager.Instance.CreatePortRule(
+                    @"TestFirewallPortRule",
+                    FirewallAction.Allow,
+                    9999,
+                    FirewallProtocol.TCP
+                );
+                FirewallManager.Instance.Rules.Add(rule);
+
+                rule = FirewallManager.Instance.CreateApplicationRule(
+                    @"TestFirewallAppRule",
+                    FirewallAction.Allow,
+                    @"C:\MyApp.exe"
+                );
+                rule.Direction = FirewallDirection.Outbound;
+                FirewallManager.Instance.Rules.Add(rule);
+
+                fwc = new FirewallCollector(SecondRunId);
+                fwc.Execute();
+
+                rule = FirewallManager.Instance.Rules.SingleOrDefault(r => r.Name == "TestFirewallPortRule");
+                if (rule != null)
+                {
+                    FirewallManager.Instance.Rules.Remove(rule);
+                }
+
+                rule = FirewallManager.Instance.Rules.SingleOrDefault(r => r.Name == "TestFirewallAppRule");
+                if (rule != null)
+                {
+                    FirewallManager.Instance.Rules.Remove(rule);
+                }
+
+                BaseCompare bc = new BaseCompare();
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                if (!bc.TryCompare(FirstRunId, SecondRunId))
+                {
+                    Assert.Fail();
+                }
+
+                Dictionary<string, List<CompareResult>> results = bc.Results;
+                Assert.IsTrue(results["FIREWALL_CREATED"].Where(x => x.Identity.Contains("80")).Count() > 0);
+                Assert.IsTrue(results["FIREWALL_CREATED"].Where(x => x.Identity.Contains("MyApp.exe")).Count() > 0);
+            }
+        }
     }
 }
