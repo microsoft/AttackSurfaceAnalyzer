2e361f3284b847b29b921172ef62ffcc329be4d3
gfs <gfs@pm.me>
2019-09-18 20:45:51 -0700

    Improve Windows Service collector. Now collects using WMI (gathers more data)

--- Lib/Objects/ServiceObject.cs
+++ Lib/Objects/ServiceObject.cs
@@ -6,10 +6,32 @@ namespace AttackSurfaceAnalyzer.Objects
 {
     public class ServiceObject : CollectObject
     {
-        public string ServiceName { get; set; }
-        public string StartType { get; set; }
+        public bool AcceptPause { get; set; }
+        public bool AcceptStop { get; set; }
+        public string Caption { get; set; }
+        public uint CheckPoint { get; set; }
+        public string CreationClassName { get; set; }
+        public bool DelayedAutoStart { get; set; }
+        public string Description { get; set; }
+        public bool DesktopInteract { get; set; }
         public string DisplayName { get; set; }
-        public string CurrentState { get; set; }
+        public string ErrorControl { get; set; }
+        public uint ExitCode { get; set; }
+        public string InstallDate { get; set; }
+        public string Name { get; set; }
+        public string PathName { get; set; }
+        public uint ProcessId { get; set; }
+        public uint ServiceSpecificExitCode { get; set; }
+        public string ServiceType { get; set; }
+        public bool Started { get; set; }
+        public string StartMode { get; set; }
+        public string StartName { get; set; }
+        public string State { get; set; }
+        public string Status { get; set; }
+        public string SystemCreationClassName { get; set; }
+        public string SystemName { get; set; }
+        public uint TagId { get; set; }
+        public uint WaitHint { get; set; }
 
         public ServiceObject()
         {
@@ -20,7 +42,7 @@ namespace AttackSurfaceAnalyzer.Objects
         {
             get
             {
-                return ServiceName;
+                return Name;
             }
         }
     }
