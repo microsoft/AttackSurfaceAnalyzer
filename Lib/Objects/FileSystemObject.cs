9228ddf50fd7832c11e20738e72ab501ed848318
gfs <gfs@pm.me>
2019-09-19 00:35:27 -0700

    Rearrange properties in File system object to make JSON output a bit prettier.

--- Lib/Objects/FileSystemObject.cs
+++ Lib/Objects/FileSystemObject.cs
@@ -7,32 +7,32 @@ namespace AttackSurfaceAnalyzer.Objects
 {
     public class FileSystemObject : CollectObject
     {
-        public string Path { get; set; }
-        public string PermissionsString { get; set; }
-        public List<KeyValuePair<string, string>> Permissions { get; set; }
-        public ulong Size { get; set; }
-        public string ContentHash { get; set; }
-        public List<string> Characteristics { get; set; }
-        public string SignatureStatus { get; set; }
-        public bool IsExecutable { get; set; }
-        public bool IsDirectory { get; set; }
-
-        public string Owner { get; set; }
-        public string Group { get; set; }
-        public bool SetGid { get; set; }
-        public bool SetUid { get; set; }
-
-        public FileSystemObject()
-        {
-            ResultType = RESULT_TYPE.FILE;
-        }
-
         public override string Identity
         {
             get
             {
                 return Path;
             }
+        }
+        public string Path { get; set; }
+        public ulong Size { get; set; }
+        public string SignatureStatus { get; set; }
+        public string ContentHash { get; set; }
+        public bool IsExecutable { get; set; }
+        public bool IsDirectory { get; set; }
+        public string Owner { get; set; }
+        public string Group { get; set; }
+        public bool SetGid { get; set; }
+        public bool SetUid { get; set; }
+        public string PermissionsString { get; set; }
+        public List<string> Characteristics { get; set; }
+
+        public List<KeyValuePair<string, string>> Permissions { get; set; }
+
+
+        public FileSystemObject()
+        {
+            ResultType = RESULT_TYPE.FILE;
         }
     }
 }
