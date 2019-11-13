c9892146b8622872cb19921607ff182c6b3df28b
gfs <gfs@pm.me>
2019-11-13 02:06:37 -0800

    Fix #310. Fix formatting in default analysis ruleset.

--- Lib/Objects/Rule.cs
+++ Lib/Objects/Rule.cs
@@ -10,26 +10,25 @@ namespace AttackSurfaceAnalyzer.Objects
 
     public class Rule
     {
-        [DefaultValue(new PLATFORM[] { PLATFORM.LINUX, PLATFORM.MACOS, PLATFORM.WINDOWS })]
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
         public List<PLATFORM> Platforms { get; }
 
-        [DefaultValue(new CHANGE_TYPE[] { CHANGE_TYPE.CREATED, CHANGE_TYPE.DELETED, CHANGE_TYPE.MODIFIED })]
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
         public List<CHANGE_TYPE> ChangeTypes { get; }
 
         public string Name { get; set; }
         public string Description { get; set; }
         public ANALYSIS_RESULT_TYPE Flag { get; set; }
         public RESULT_TYPE ResultType { get; set; }
-        public List<Clause> Clauses { get; }
+
+        public List<Clause> Clauses { get; set; }
     }
 
     public class Clause
     {
         public string Field { get; set; }
         public OPERATION Operation { get; set; }
-        public List<string> Data { get; }
-        public List<KeyValuePair<string, string>> DictData { get; }
+
+        public List<string> Data { get; set; }
+
+        public List<KeyValuePair<string, string>> DictData { get; set; }
     }
 }
