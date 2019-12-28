d8cffea7f313904c416371e3a4b06fa79ebfabef
gfs <gfs@pm.me>
2019-12-27 19:03:53 -0800

    Remove extra using.

--- Lib/Objects/FileSystemObject.cs
+++ Lib/Objects/FileSystemObject.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 using AttackSurfaceAnalyzer.Types;
-using Mono.Unix;
 using System.Collections.Generic;
 
 namespace AttackSurfaceAnalyzer.Objects
