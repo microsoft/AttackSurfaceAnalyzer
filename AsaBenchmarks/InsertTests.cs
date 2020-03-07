37a1204b192c3a039d334c02475d8e3ed06898f6
Gabe Stocco <gstocco@microsoft.com>
2020-03-06 18:31:43 -0800

    Dynamically create appropriately sized databases

--- AsaBenchmarks/InsertTests.cs
+++ AsaBenchmarks/InsertTests.cs
@@ -13,42 +13,36 @@ namespace AttackSurfaceAnalyzer.Benchmarks
 {
     public class InsertTests
     {
-        [Params(10000, 25000)]
+        [Params(10000)]
         public int N { get; set; }
 
-        [Params(1, 4)]
+        [Params(0)]
+        public int StartingSize { get; set; }
+
+        [Params(0, 50, 100, 200, 300)]
+        public int ObjectSize { get; set; }
+
+        [Params(12)]
         public int Shards { get; set; }
+
+        // Bag of reusable objects to write to the database.
+        private readonly ConcurrentBag<FileSystemObject> BagOfObjects = new ConcurrentBag<FileSystemObject>();
+        private readonly Random rnd = new Random();
 
         public InsertTests()
         {
+            Logger.Setup(true, true);
             Strings.Setup();
         }
 
         [Benchmark]
-        public void RunInsertTest()
+        public void Insert_N_Objects()
         {
-            DatabaseManager.BeginTransaction();
-
             Parallel.For(0, N, i =>
             {
-                DatabaseManager.Write(new FileSystemObject()
-                {
-                    Characteristics = new List<string>() { "One", "Two", "Three", "Four" },
-                    FileType = $"{i}",
-                    Group = "Wheel",
-                    IsDirectory = false,
-                    IsExecutable = true,
-                    IsLink = false,
-                    Owner = "Microsoft",
-                    Path = "/bin/AttackSurfaceAnalyzer",
-                    Permissions = new Dictionary<string, string>() { { "Owner", "ReadWriteExecute" }, { "Group", "ReadWriteExecute" }, { "Everyone", "ReadWriteExecute" } },
-                    PermissionsString = "This is a fake permissions string",
-                    SetGid = false,
-                    SetUid = false,
-                    ResultType = Types.RESULT_TYPE.FILE,
-                    SignatureStatus = new Signature() { IsAuthenticodeValid = false },
-                    Size = 300
-                }, $"InsertTest_Sharding");
+                var obj = GetRandomObject();
+                DatabaseManager.Write(obj, $"InsertTest_Sharding");
+                BagOfObjects.Add(obj);
             });
 
             while (DatabaseManager.HasElements())
@@ -56,14 +50,85 @@ namespace AttackSurfaceAnalyzer.Benchmarks
                 Thread.Sleep(1);
             }
 
+        }
+
+        [Benchmark]
+        public FileSystemObject GetRandomObject()
+        {
+            BagOfObjects.TryTake(out FileSystemObject obj);
+
+            if (obj != null)
+            {
+                obj.Path = $"/bin/AttackSurfaceAnalyzer_{rnd.Next()}";
+                return obj;
+            }
+            else
+            {
+                return new FileSystemObject()
+                {
+                    Characteristics = new List<string>() { "One", "Two", "Three", "Four" },
+                    // Pad this field with extra data.  The ObjectSize parameter determines the size of this data.
+                    FileType = Enumerable.Repeat("a", ObjectSize).ToString(),
+                    Group = "Wheel",
+                    IsDirectory = false,
+                    IsExecutable = true,
+                    IsLink = false,
+                    Owner = "Microsoft",
+                    Path = $"/bin/AttackSurfaceAnalyzer_{rnd.Next()}",
+                    Permissions = new Dictionary<string, string>() { { "Owner", "ReadWriteExecute" }, { "Group", "ReadWriteExecute" }, { "Everyone", "ReadWriteExecute" } },
+                    PermissionsString = "This is a fake permissions string",
+                    SetGid = false,
+                    SetUid = false,
+                    ResultType = Types.RESULT_TYPE.FILE,
+                    SignatureStatus = new Signature() { IsAuthenticodeValid = false },
+                    Size = 300
+                };
+            }
+        }
+
+        public void PopulateDatabases()
+        {
+            DatabaseManager.BeginTransaction();
+
+            Parallel.For(0, N, i =>
+            {
+                DatabaseManager.Write(GetRandomObject(), $"InsertTest_Sharding");
+            });
+
+            while (DatabaseManager.HasElements())
+            {
+                Thread.Sleep(100);
+            }
+
             DatabaseManager.Commit();
         }
 
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            DatabaseManager.Setup(filename: $"AsaBenchmark_{Shards}.sqlite", shardingFactor: Shards);
+            PopulateDatabases();
+            DatabaseManager.CloseDatabase();
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            DatabaseManager.Destroy();
+        }
+
         [IterationSetup]
-        public void IterationSetup() => DatabaseManager.Setup(filename: "AsaBenchmark.sqlite", shardingFactor: Shards);
+        public void IterationSetup()
+        {
+            DatabaseManager.Setup(filename: $"AsaBenchmark_{Shards}.sqlite", shardingFactor: Shards);
+            DatabaseManager.BeginTransaction();
+        }
 
         [IterationCleanup]
-        public void IterationCleanup() => DatabaseManager.Destroy();
-
+        public void IterationCleanup()
+        {
+            DatabaseManager.RollBack();
+            DatabaseManager.CloseDatabase();
+        }
     }
 }
--- AsaBenchmarks/Program.cs
+++ AsaBenchmarks/Program.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+using AttackSurfaceAnalyzer.Objects;
 using AttackSurfaceAnalyzer.Utils;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
@@ -14,46 +17,7 @@ namespace AttackSurfaceAnalyzer.Benchmarks
     {
         public static void Main(string[] args)
         {
-            Logger.Setup(true, true);
-            Strings.Setup();
-
             var summary = BenchmarkRunner.Run<InsertTests>();
-
-            //var InsertTests = new InsertTests();
-            //InsertTests.N = 10000;
-            //var MaxThreads = 12;
-            //var Iterations = 100;
-
-            //var results = new List<List<TimeSpan>>();
-
-            //for (int threads = 1; threads < MaxThreads; threads++)
-            //{
-            //    Log.Information($"Running {threads} shards.");
-            //    results.Add(new List<TimeSpan>());
-            //    for (int i = 0; i < Iterations; i++)
-            //    {
-            //        DatabaseManager.Setup(filename: "AsaBenchmark.sqlite", shardingFactor: threads);
-
-            //        var StopWatch = System.Diagnostics.Stopwatch.StartNew();
-
-            //        InsertTests.RunInsertTest();
-
-            //        StopWatch.Stop();
-
-            //        DatabaseManager.Destroy();
-
-            //        TimeSpan t = TimeSpan.FromMilliseconds(StopWatch.ElapsedMilliseconds);
-
-            //        string answer = string.Format(CultureInfo.InvariantCulture, "{0:D2}h:{1:D2}m:{2:D2}s:{3:D3}ms",
-            //            t.Hours,
-            //            t.Minutes,
-            //            t.Seconds,
-            //            t.Milliseconds);
-            //        Log.Information("Completed in {0}", answer);
-            //        results[threads - 1].Add(t);
-            //    }
-            //}
-            Log.Information(Utf8Json.JsonSerializer.ToJsonString(summary));
         }
     }
 }
