1a9e682dcd4a6cf7b698d6251916aa73e65a08b8
Gabe Stocco <gstocco@microsoft.com>
2020-03-10 11:03:10 -0700

    Fix accidentally deleting entries from populate database

--- AsaBenchmarks/InsertTests.cs
+++ AsaBenchmarks/InsertTests.cs
@@ -17,17 +17,16 @@ namespace AttackSurfaceAnalyzer.Benchmarks
 
         // The number of records to populate the database with before the benchmark
         //[Params(0,100000,200000,400000,800000,1600000,3200000)]
-        [Params(1000000)]
+        [Params(0)]
         public int StartingSize { get; set; }
 
         // The amount of padding to add to the object in bytes
         // Default size is approx 530 bytes serialized
         // Does not include SQL overhead
-        [Params(0)]
+        [Params(0,500)]
         public int ObjectPadding { get; set; }
 
         // The number of Shards/Threads to use for Database operations
-        //[Params(1,2,3,4,5,6,7,8,9,10,12)]
         [Params(1,2,3,4,5,6,7,8,9,10,11,12)]
         public int Shards { get; set; }
 
@@ -41,14 +40,14 @@ namespace AttackSurfaceAnalyzer.Benchmarks
         }
 
         [Benchmark]
-        public void Insert_N_Objects() => Insert_X_Objects(N, ObjectPadding);
+        public void Insert_N_Objects() => Insert_X_Objects(N, ObjectPadding, "Insert_N_Objects");
 
-        public static void Insert_X_Objects(int X, int ObjectPadding = 0)
+        public static void Insert_X_Objects(int X, int ObjectPadding = 0, string runName = "Insert_X_Objects")
         {
             Parallel.For(0, X, i =>
             {
                 var obj = GetRandomObject(ObjectPadding);
-                DatabaseManager.Write(obj, $"Insert_N_Objects");
+                DatabaseManager.Write(obj, runName);
                 BagOfObjects.Add(obj);
             });
 
@@ -83,7 +82,7 @@ namespace AttackSurfaceAnalyzer.Benchmarks
             DatabaseManager.Setup(filename: $"AsaBenchmark_{Shards}.sqlite", shardingFactor: Shards);
             DatabaseManager.BeginTransaction();
 
-            Insert_X_Objects(StartingSize);
+            Insert_X_Objects(StartingSize,ObjectPadding,"PopulateDatabase");
 
             DatabaseManager.Commit();
             DatabaseManager.CloseDatabase();
