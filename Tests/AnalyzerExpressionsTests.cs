3aad5128ec69c77ff4b6147ae51044a7b1c3e610
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-04-10 13:35:36 -0700

    Update AnalyzerExpressionsTests.cs

--- Tests/AnalyzerExpressionsTests.cs
+++ Tests/AnalyzerExpressionsTests.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace AttackSurfaceAnalyzer.Tests
 {
@@ -18,7 +17,7 @@ namespace AttackSurfaceAnalyzer.Tests
         public void Setup()
         {
             Logger.Setup(false, true);
-            Strings.Setup();
+            Utils.Strings.Setup();
             AsaTelemetry.Setup(test: true);
             DatabaseManager.Setup(Path.GetTempFileName());
         }
