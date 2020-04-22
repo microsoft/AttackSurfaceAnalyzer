feababad8b88431356cf72ee257ae2ac0400f567
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-04-21 19:20:10 -0700

    Add skeleton for TPM Collector

---
+++ Lib/Collectors/TpmCollector.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using System.Runtime.InteropServices;
+using AttackSurfaceAnalyzer.Objects;
+using AttackSurfaceAnalyzer.Utils;
+using Serilog;
+using Tpm2Lib;
+
+namespace AttackSurfaceAnalyzer.Collectors
+{
+    /// <summary>
+    /// Collects metadata from the local certificate stores.
+    /// </summary>
+    public class TpmCollector : BaseCollector
+    {
+        public TpmCollector()
+        {
+        }
+
+        public override bool CanRunOnPlatform()
+        {
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        }
+
+        /// <summary>
+        /// Execute the certificate collector.
+        /// </summary>
+        public override void ExecuteInternal()
+        {
+            Tpm2Device? tpmDevice = null;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                tpmDevice = new TbsDevice();
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                tpmDevice = new LinuxTpmDevice();
+            }
+
+            if (tpmDevice is Tpm2Device)
+            {
+                //
+            }
+
+            tpmDevice?.Dispose();
+        }
+    }
+}
