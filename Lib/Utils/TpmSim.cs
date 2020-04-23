eb0864539dcce1f61680bbf2224032dcad90b431
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-04-22 19:49:05 -0700

    Create TpmSim.cs

---
+++ Lib/Utils/TpmSim.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AttackSurfaceAnalyzer.Utils
+{
+    public static class TpmSim
+    {
+        [DllImport("Tpm.Dll")]
+        static extern int StartTcpServer(int port);
+    }
+}
