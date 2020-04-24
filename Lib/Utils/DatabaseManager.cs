4e3ca3c3640afc40421314bf1eef7c3166cbdecd
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-04-24 07:40:50 -0700

    Store the absolute path of the database in DatabaseManager.

--- Lib/Utils/DatabaseManager.cs
+++ Lib/Utils/DatabaseManager.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -137,7 +138,14 @@ namespace AttackSurfaceAnalyzer.Utils
             {
                 if (SqliteFilename != filename)
                 {
-                    SqliteFilename = filename;
+                    if (Path.IsPathRooted(filename))
+                    {
+                        SqliteFilename = filename;
+                    }
+                    else
+                    {
+                        SqliteFilename = $"{Directory.GetCurrentDirectory()}{Path.DirectorySeparatorChar}{filename}";
+                    }
                 }
             }
 
