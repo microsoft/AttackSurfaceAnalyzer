6d7e0d89ed2c4e96775892ea12ddc83f7f13423b
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-05-06 16:19:58 -0700

    Add extra test case to validate diff is working.

--- Tests/ComparatorTests.cs
+++ Tests/ComparatorTests.cs
@@ -99,6 +99,7 @@ namespace AttackSurfaceAnalyzer.Tests
             Assert.IsTrue(results[(RESULT_TYPE.FILE, CHANGE_TYPE.CREATED)].Any(x => x.Compare is FileSystemObject FSO && FSO.Identity.Contains("TestPath3") && FSO.Size == 701));
             Assert.IsTrue(results[(RESULT_TYPE.FILE, CHANGE_TYPE.DELETED)].Any(x => x.Compare is FileSystemObject FSO && FSO.Identity.Contains("TestPath") && FSO.Size == 701));
             Assert.IsTrue(results[(RESULT_TYPE.FILE, CHANGE_TYPE.MODIFIED)].Any(x => x.Compare is FileSystemObject FSO && x.Base is FileSystemObject FSO2 && FSO.Identity.Contains("TestPath4") && FSO.IsExecutable == true && FSO2.IsExecutable == false));
+            Assert.IsTrue(results[(RESULT_TYPE.FILE, CHANGE_TYPE.MODIFIED)].Any(x => x.Diffs.Any(y => y.Field == "IsExecutable") && x.Identity.Contains("TestPath4")));
             Assert.IsFalse(results[(RESULT_TYPE.FILE, CHANGE_TYPE.MODIFIED)].Any(x => x.Identity.Contains("TestPath2")));
         }
 
