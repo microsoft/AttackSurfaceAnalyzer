da8c423e15a420a4e3955c4129523e6ab18792f4
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-06-24 22:14:02 -0700

    Fix Rehydration of ProcessObjects

--- Lib/Objects/ProcessModuleObject.cs
+++ Lib/Objects/ProcessModuleObject.cs
@@ -7,28 +7,27 @@ namespace AttackSurfaceAnalyzer.Objects
 {
     public class ProcessModuleObject
     {
-        public string FileName { get; }
-
-        public string ModuleName { get; }
-
-        public FileVersionInfo FileVersionInfo { get; }
-
-        public ProcessModuleObject(string FileName, string ModuleName, FileVersionInfo FileVersionInfo)
+        public ProcessModuleObject(string FileName, string ModuleName, SerializableFileVersionInfo? FileVersionInfo)
         {
             this.FileName = FileName;
             this.ModuleName = ModuleName;
             this.FileVersionInfo = FileVersionInfo;
         }
 
+        public string FileName { get; }
+
+        public SerializableFileVersionInfo? FileVersionInfo { get; }
+        public string ModuleName { get; }
+
         internal static ProcessModuleObject FromProcessModule(ProcessModule mainModule)
         {
-            return new ProcessModuleObject(mainModule.FileName, mainModule.ModuleName, mainModule.FileVersionInfo);
+            return new ProcessModuleObject(mainModule.FileName, mainModule.ModuleName, SerializableFileVersionInfo.FromFileVersionInfo(mainModule.FileVersionInfo));
         }
 
         internal static List<ProcessModuleObject> FromProcessModuleCollection(ProcessModuleCollection modules)
         {
             var output = new List<ProcessModuleObject>();
-            foreach(var processModule in modules)
+            foreach (var processModule in modules)
             {
                 if (processModule is ProcessModule pm)
                     output.Add(FromProcessModule(pm));
