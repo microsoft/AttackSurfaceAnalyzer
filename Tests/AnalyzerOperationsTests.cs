da8b8438559c3fcda96185b46a4d8a21fc7e0d97
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-07-09 08:20:31 -0700

    Update AnalyzerOperationsTests.cs

--- Tests/AnalyzerOperationsTests.cs
+++ Tests/AnalyzerOperationsTests.cs
@@ -6,7 +6,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Management.Automation;
 
 namespace AttackSurfaceAnalyzer.Tests
 {
