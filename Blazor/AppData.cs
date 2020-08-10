daabcfb6ec0aa1db2ef89e01c719b95c24424f0d
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-08-10 10:59:24 -0700

    WIP Blazor Interface

---
+++ Blazor/AppData.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.CST.AttackSurfaceAnalyzer.Cli
+{
+    public class AppData
+    {
+        public CollectCommandOptions CollectOptions { get; set; } = new CollectCommandOptions();
+        public MonitorCommandOptions MonitorOptions { get; set; } = new MonitorCommandOptions();
+        public ExportCollectCommandOptions ExportCollectCommandOptions { get; set; } = new ExportCollectCommandOptions();
+    }
+}
