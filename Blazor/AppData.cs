b91889f02d24e510042e461a2478bba93b609e23
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-08-13 11:56:04 -0700

    Component breakdowns

--- Blazor/AppData.cs
+++ Blazor/AppData.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.CST.AttackSurfaceAnalyzer.Objects;
+using System.Collections.Generic;
 
 namespace Microsoft.CST.AttackSurfaceAnalyzer.Cli
 {
@@ -7,7 +8,31 @@ namespace Microsoft.CST.AttackSurfaceAnalyzer.Cli
         public CollectCommandOptions CollectOptions { get; set; } = new CollectCommandOptions();
         public MonitorCommandOptions MonitorOptions { get; set; } = new MonitorCommandOptions();
         public ExportCollectCommandOptions ExportCollectCommandOptions { get; set; } = new ExportCollectCommandOptions();
+        public string FirstRunId
+        {
+            get
+            {
+                return $"{RunId}-before";
+            }
+        }
 
+        public string SecondRunId
+        {
+            get
+            {
+                return $"{RunId}-after";
+            }
+        }
+
+        public string MonitorRunId
+        {
+            get
+            {
+                return $"{RunId}-monitoring";
+            }
+        }
+
+        public string RunId { get; set; } = string.Empty;
         public CompareCommandOptions CompareCommandOptions { 
             get
             {
