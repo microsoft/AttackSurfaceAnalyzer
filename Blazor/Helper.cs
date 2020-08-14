a9a28b63443487ebd19832a67171f32b5d5d8561
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-08-13 19:05:20 -0700

    Redo Glow

---
+++ Blazor/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.CST.AttackSurfaceAnalyzer.Cli
+{
+    public static class Helper
+    {
+        public static string GetGlowClass(bool value)
+        {
+            return value ? "glowRed" : "glowBlue";
+        }
+    }
+}
