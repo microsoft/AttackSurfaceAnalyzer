c5aa6e41f2f81c3270ce0f93dcaf6d756817bb4b
Gabe Stocco <98900+gfs@users.noreply.github.com>
2021-08-03 09:01:50 -0700

    Add Comments to Rule File

--- Lib/Objects/RuleFile.cs
+++ Lib/Objects/RuleFile.cs
@@ -13,20 +13,48 @@ namespace Microsoft.CST.AttackSurfaceAnalyzer.Objects
 {
     public class RuleFile
     {
-        public RuleFile(Dictionary<RESULT_TYPE, ANALYSIS_RESULT_TYPE>? DefaultLevels = null, List<AsaRule>? Rules = null)
+        /// <summary>
+        /// Create a RuleFile with provided DefaultLevels, Rules and Source name.
+        /// </summary>
+        /// <param name="DefaultLevels"></param>
+        /// <param name="Rules"></param>
+        /// <param name="Source"></param>
+        [JsonConstructor]
+        public RuleFile(Dictionary<RESULT_TYPE, ANALYSIS_RESULT_TYPE>? DefaultLevels = null, IEnumerable<AsaRule>? Rules = null, string? Source = null)
         {
             this.DefaultLevels = DefaultLevels ?? this.DefaultLevels;
             this.Rules = Rules ?? new List<AsaRule>();
+            this.Source = Source;
         }
 
+        /// <summary>
+        /// Create a RuleFile with the default Default Levels and null Source name.
+        /// </summary>
+        /// <param name="Rules"></param>
+        public RuleFile(IEnumerable<AsaRule>? Rules = null) : this(null, Rules, null)
+        {
+        }
+
+        /// <summary>
+        /// Create an empty RuleFile.
+        /// </summary>
         public RuleFile()
         {
         }
 
-        public string? Source { get; set; } // An Identifier for the source of the rules
+        /// <summary>
+        /// An Identifier for the source of the Rules
+        /// </summary>
+        public string? Source { get; set; }
 
+        /// <summary>
+        /// The List of Rules
+        /// </summary>
         public IEnumerable<AsaRule> Rules { get; set; } = new List<AsaRule>();
 
+        /// <summary>
+        /// The Default Levels to apply to objects if there is no corresponding rule.
+        /// </summary>
         public Dictionary<RESULT_TYPE, ANALYSIS_RESULT_TYPE> DefaultLevels { get; set; } = new Dictionary<RESULT_TYPE, ANALYSIS_RESULT_TYPE>()
         {
             { RESULT_TYPE.CERTIFICATE, ANALYSIS_RESULT_TYPE.INFORMATION },
@@ -46,7 +74,13 @@ namespace Microsoft.CST.AttackSurfaceAnalyzer.Objects
             { RESULT_TYPE.FILEMONITOR, ANALYSIS_RESULT_TYPE.INFORMATION }
         };
 
-        public static RuleFile FromStream(Stream? stream)
+        /// <summary>
+        /// Generate a RuleFile from a given stream containing a serialized RuleFile.
+        /// </summary>
+        /// <param name="stream">The Stream to Deserialize</param>
+        /// <param name="streamName">The Source Name to set in the RuleFile</param>
+        /// <returns></returns>
+        public static RuleFile FromStream(Stream? stream, string? streamName)
         {
             if (stream is null)
                 throw new NullReferenceException(nameof(stream));
@@ -55,8 +89,7 @@ namespace Microsoft.CST.AttackSurfaceAnalyzer.Objects
                 using (StreamReader file = new StreamReader(stream))
                 {
                     var config = JsonConvert.DeserializeObject<RuleFile>(file.ReadToEnd());
-                    if (config.Source is null)
-                        config.Source = "Stream";
+                    config.Source = streamName ?? (config.Source ?? "Stream");
                     return config;
                 }
             }
@@ -75,11 +108,17 @@ namespace Microsoft.CST.AttackSurfaceAnalyzer.Objects
             return new RuleFile();
         }
 
-        public string GetHash()
-        {
-            return CryptoHelpers.CreateHash(JsonConvert.SerializeObject(this));
-        }
+        /// <summary>
+        /// Get the Hash of the RuleFile
+        /// </summary>
+        /// <returns></returns>
+        public string GetHash() => CryptoHelpers.CreateHash(JsonConvert.SerializeObject(this));
 
+        /// <summary>
+        /// Load rules from a serialized RuleFile on disk.
+        /// </summary>
+        /// <param name="filterLoc"></param>
+        /// <returns></returns>
         public static RuleFile FromFile(string? filterLoc = "")
         {
             if (!string.IsNullOrEmpty(filterLoc))
@@ -89,8 +128,7 @@ namespace Microsoft.CST.AttackSurfaceAnalyzer.Objects
                     using (StreamReader file = System.IO.File.OpenText(filterLoc))
                     {
                         var config = JsonConvert.DeserializeObject<RuleFile>(file.ReadToEnd());
-                        if (config.Source is null)
-                            config.Source = filterLoc;
+                        config.Source = filterLoc;
                         return config;
                     }
                 }
@@ -110,6 +148,10 @@ namespace Microsoft.CST.AttackSurfaceAnalyzer.Objects
             return new RuleFile();
         }
 
+        /// <summary>
+        /// Load the default AttackSurfaceAnalyzer Rules embedded in the binary.
+        /// </summary>
+        /// <returns></returns>
         public static RuleFile LoadEmbeddedFilters()
         {
             try
@@ -117,8 +159,7 @@ namespace Microsoft.CST.AttackSurfaceAnalyzer.Objects
                 var assembly = typeof(FileSystemObject).Assembly;
                 var resourceName = "AttackSurfaceAnalyzer.analyses.json";
                 using Stream stream = assembly.GetManifestResourceStream(resourceName) ?? throw new NullReferenceException($"assembly.GetManifestResourceStream couldn't load {resourceName}");
-                var file = FromStream(stream);
-                file.Source = "Embedded Rules";
+                var file = FromStream(stream, "Embedded Rules");
                 return file;
             }
             catch (Exception e) when (
@@ -135,20 +176,13 @@ namespace Microsoft.CST.AttackSurfaceAnalyzer.Objects
             return new RuleFile();
         }
 
+        /// <summary>
+        /// Print a serialization of the filters to the verbose console.
+        /// </summary>
         public void DumpFilters()
         {
             Log.Verbose("Filter dump:");
             Log.Verbose(JsonConvert.SerializeObject(this));
-        }
-
-        public List<Rule> GetRules()
-        {
-            return Rules.Select(x => (Rule)x).ToList(); ;
-        }
-
-        public List<Rule> GetRulesForPlatform(PLATFORM platform)
-        {
-            return (List<Rule>)Rules.Where(x => x.Platforms.Contains(platform) || !x.Platforms.Any());
         }
     }
 }
