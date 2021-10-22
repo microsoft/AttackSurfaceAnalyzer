69926204d130258a41b5c3a4469e4c4dfcf7c44d
Shaopeng <81775155+shaopeng-gh@users.noreply.github.com>
2021-10-21 18:25:20 -0700

    Add methods to support Sarif format (#622)

--- Lib/Objects/CommandOptions.cs
+++ Lib/Objects/CommandOptions.cs
@@ -245,6 +245,9 @@ namespace Microsoft.CST.AttackSurfaceAnalyzer
 
         [Option(HelpText = "Enable running Scripts in rules")]
         public bool RunScripts { get; set; }
+
+        [Option(HelpText = "Output Sarif")]
+        public bool OutputSarif { get; set; }
     }
 
     [Verb("gui", HelpText = "Launch the GUI in a browser.")]
@@ -291,6 +294,9 @@ namespace Microsoft.CST.AttackSurfaceAnalyzer
 
         [Option(HelpText = "Run Scripts")]
         public bool RunScripts { get; set; }
+
+        [Option(HelpText = "Export Sarif")]
+        public bool ExportSarif { get; set; }
     }
 
     [Verb("monitor", HelpText = "Continue running and monitor activity")]
