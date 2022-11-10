b08ffea31e7dacff87bd6b67839551fc78edbb1d
Gabe Stocco <98900+gfs@users.noreply.github.com>
2022-11-09 16:24:46 -0800

    Fix Saving Rules

---
+++ Cli/wwwroot/js/SaveFile.js
@@ -0,0 +1,8 @@
+﻿function FileSaveAs(filename, fileContent) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:text/plain;charset=utf-8," + encodeURIComponent(fileContent)
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
