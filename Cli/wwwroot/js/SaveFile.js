613d72406b448da140f65ec4310b482d2222d60f
Gabe Stocco <98900+gfs@users.noreply.github.com>
2022-11-21 14:29:27 -0800

    Fix Saving Rules
    Fixes saving rules in the GUI.
    
    Fix #675.
    
    Needs one more commit after updated OAT is published which will prevent serialization of extraneous properties.

--- Cli/wwwroot/js/SaveFile.js
+++ Cli/wwwroot/js/SaveFile.js
@@ -1,7 +1,7 @@
-﻿function FileSaveAs(filename, fileContent) {
+﻿function FileSaveAs(filename, encoding, fileContent) {
     var link = document.createElement('a');
     link.download = filename;
-    link.href = "data:text/plain;charset=utf-8," + encodeURIComponent(fileContent)
+    link.href = encoding + encodeURIComponent(fileContent)
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
